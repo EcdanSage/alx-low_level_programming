b7d8355d0ed59d60093213344e919622331d8ef1
EcdanSage <ecdan94@gmail.com>
2022-04-26 16:41:18 -0700

    Task 9 Even more pointers

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,10 +1,10 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- * set_string - sets the value of a pointer to a char
- * @s: source adress.
- * @to: target adress.
- * Return: no return.
+ * set_string - sets the value of a pointer to a char.
+ * @s: double pointer to a string
+ * @to: pointer to the string to set s to
+ * Return: description of the returned value.
  */
 void set_string(char **s, char *to)
 {
