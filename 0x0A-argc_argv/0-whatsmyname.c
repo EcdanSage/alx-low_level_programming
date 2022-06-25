30cfa537b12ff61aba003ce5ef566502a25fcd48
root <ecdan94@gmail.com>
2022-06-24 17:10:46 -0700

    Task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,18 +1,16 @@
 #include "main.h"
 /**
- * _puts_recursion - Prints a string followed by a new line.
+ * _puts_recursion - prints a string folowed by a new line
  * @s: string
- *
- * Return: On success 1.
- * On errer, -1 is returned, and errno is set appropriately.
+ * Return: Always (0)
  */
 void _puts_recursion(char *s)
 {
-	if (*s == 0)
-	{
+	if (*s == '\0')
 		_putchar('\n');
-		return;
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(++s);
 	}
-	_putchar(*s);
-	_puts_recursion(s + 1);
 }
